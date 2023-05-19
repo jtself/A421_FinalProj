b3874318fa4e1d896256f7eeaa0b4c60c27cb0a9
Justin Self <jtself@calpoly.edu>
2023-05-19 14:57:30 -0700

    td working!

--- quat2rotationmatrix.m
+++ quat2rotationmatrix.m
@@ -3,7 +3,9 @@
     %{
     quaternion to associated rotation matrix function
     %}
+    ep = epsilon;
+    epsilon_cross = [     0       -ep(3)     ep(2);
+                          ep(3)       0      -ep(1);
+                         -ep(2)    ep(1)       0   ];
     
-    epsilon_cross = vector_cross(epsilon);
-    
-    C21 = (2*eta^2 - 1)*eye(3) + 2*(epsilon*epsilon') - 2*eta*epsilon_cross;
+    C21 = (2*eta^2 - 1)*eye(3) + 2*(ep*ep') - 2*eta*epsilon_cross;
